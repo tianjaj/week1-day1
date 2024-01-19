41ca0c0075b269472a4915edae10819862289a10
tianjaj <hutianhang1108@gmail.com>
2024-01-19 00:32:28 -0500

    wek1d1

---
+++ m3-w1-day1/node-example2a.js
@@ -0,0 +1,5 @@
+const circle = require('./node-example2.js');
+
+//console.log(`The area of a circle 4 is {circle.area(4)}); //concatenation
+console.log(`The area of a circle 4 is ${circle.area(4)}`); //
+
